5c17234d05e3057c32de35d4acfec8096261731f
gauravneu <99050945+gauravneu@users.noreply.github.com>
2022-02-17 19:26:59 -0500

    Second assignment

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,201 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author gaurav
+ */
+public class Car {
+    
+private String manufacturer;
+private int serialNumber;
+private int modelNumber;
+private String color;
+private int manufactureYear;
+private int seatsNumber;
+
+private int licensePlates;
+private String ownerName;
+private long ownerTelephoneNumbers;
+private int ownerDriverLicense;
+private int ownerSocialSecurityNumber;
+private String ownerAddress;
+private int warrantyYear;
+private Date lastUpdateOrCreate;
+private boolean availability;
+private String city;
+private Date maintainanceCertificateDate;
+
+
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setSerialNumber(int serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public int getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(int modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getManufactureYear() {
+        return manufactureYear;
+    }
+
+    public void setManufactureYear(int manufactureYear) {
+        this.manufactureYear = manufactureYear;
+    }
+
+    public int getSeatsNumber() {
+        return seatsNumber;
+    }
+
+    public void setSeatsNumber(int seatsNumber) {
+        this.seatsNumber = seatsNumber;
+    }
+
+
+    
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public long getOwnerTelephoneNumbers() {
+        return ownerTelephoneNumbers;
+    }
+
+    public void setOwnerTelephoneNumbers(long ownerTelephoneNumbers) {
+        this.ownerTelephoneNumbers = ownerTelephoneNumbers;
+    }
+
+    public int getLicensePlates() {
+        return licensePlates;
+    }
+
+    public void setLicensePlates(int licensePlates) {
+        this.licensePlates = licensePlates;
+    }
+
+    public int getOwnerDriverLicense() {
+        return ownerDriverLicense;
+    }
+
+    public void setOwnerDriverLicense(int ownerDriverLicense) {
+        this.ownerDriverLicense = ownerDriverLicense;
+    }
+
+    public int getOwnerSocialSecurityNumber() {
+        return ownerSocialSecurityNumber;
+    }
+
+    public void setOwnerSocialSecurityNumber(int ownerSocialSecurityNumber) {
+        this.ownerSocialSecurityNumber = ownerSocialSecurityNumber;
+    }
+
+
+
+
+
+    public String getOwnerAddress() {
+        return ownerAddress;
+    }
+
+    public void setOwnerAddress(String ownerAddress) {
+        this.ownerAddress = ownerAddress;
+    }
+
+    public int getWarrantyYear() {
+        return warrantyYear;
+    }
+
+    public void setWarrantyYear(int warrantyYear) {
+        this.warrantyYear = warrantyYear;
+    }
+/*   public Date getLastUpdateOrCreate() {
+        return lastUpdateOrCreate;
+    }
+
+    public void setLastUpdateOrCreate(Date lastUpdateOrCreate) {
+        this.lastUpdateOrCreate = lastUpdateOrCreate;
+    } */
+
+    public Date getLastUpdateOrCreate() {
+        return lastUpdateOrCreate;
+    }
+
+    public void setLastUpdateOrCreate(Date lastUpdateOrCreate) {
+        this.lastUpdateOrCreate = lastUpdateOrCreate;
+    }
+
+    public Date getMaintainanceCertificateDate() {
+        return maintainanceCertificateDate;
+    }
+
+    public void setMaintainanceCertificateDate(Date maintainanceCertificateDate) {
+        this.maintainanceCertificateDate = maintainanceCertificateDate;
+    }
+
+    
+    
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean Availability) {
+        this.availability = Availability;
+    }
+
+/*    public Date getMaintainanceCertificateDate() {
+        return maintainanceCertificateDate;
+    }
+
+    public void setMaintainanceCertificateDate(Date maintainanceCertificateDate) {
+        this.maintainanceCertificateDate = maintainanceCertificateDate;
+    } */
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+        
+
+
+
+
+}
---
+++ src/main/java/model/Cars.java
@@ -0,0 +1,235 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+
+/**
+ *
+ * @author gaurav
+ */
+public class Cars {
+    private List<Car> cars;
+
+    public Cars() {
+        cars = new ArrayList<>();
+    }
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
+    
+    
+    //Add New Car
+    
+    public Car addNewcar(){
+        Car newCar = new Car();
+        cars.add(newCar);
+        return newCar;
+    }
+    
+    
+    //Search Car By Serial Number
+    
+    
+    public Car searchCarBySerialNumber(int serialNo){
+        
+        for (Car c : cars){
+            
+//            System.out.println("Cars class: get SerialNo :" + c.getSerialNumber());
+//            System.out.println("Cars class: SerialNo :" + serialNo);
+            if (c.getSerialNumber() == (serialNo)){
+                return c; 
+            }         
+        }
+        return null;
+    } 
+    
+    
+    //Searching Cars By Brand/Make
+    
+    
+    public List<Car> searchCarByManufacturer(String manufacturer){
+    List<Car> car2 = new ArrayList<>();
+    for (Car c: cars){
+        System.out.println("Loop running");
+        System.out.println("c,gm : " + c.getManufacturer());
+        System.out.println("m : "+ manufacturer);
+        if (c.getManufacturer().equals(manufacturer)){
+            System.out.println("Cars.java | searchCarByManufacturer | c.getManufacturer()  | manufacturer" + c.getManufacturer() + manufacturer);
+        car2.add(c);
+        }
+       
+    }
+        
+    return car2;
+    
+}
+    
+    //Searching Cars By ModelNumber
+    
+    /**
+     *
+     * @param modelNumber
+     * @return
+     */
+    public ArrayList<Car> searchCarByModel(int modelNumber){
+    ArrayList<Car> car2 = new ArrayList<Car>();
+    for (Car c: cars){
+        if (c.getModelNumber()== modelNumber){
+        car2.add(c);
+        }
+        
+    }
+    return car2;
+    
+}
+    
+    //Get All Cars
+    
+    public List<Car> getAllCars(){
+    return cars;
+    
+}
+    
+    //Get First Available Car
+    
+    public Car searchFirstAvailableCar(){
+        
+        for (Car c : cars){
+            if (c.isAvailability() == true){
+            return c;
+        }           
+        }
+        return null;
+    } 
+    
+    
+    //Get All Available Cars
+    
+    public ArrayList<Car> getAvailableCars(){
+    ArrayList<Car> car2 = new ArrayList<Car>();
+    for (Car c: cars){
+        if (c.isAvailability() == true){
+        car2.add(c);
+        }
+    }
+    return car2;
+    
+}
+    
+    
+        
+    //Get All Cars manufactured in given Year
+    
+    public ArrayList<Car> getCarsByManufacturedYear(int manufactureYear){
+    ArrayList<Car> car2 = new ArrayList<Car>();
+    for (Car c: cars){
+        if (c.getManufactureYear() == manufactureYear){
+        car2.add(c);
+        }
+        return car2;
+    }
+    return null;
+    
+}
+    
+        //Get All Cars available in given City
+    
+    public ArrayList<Car> getCarsByCity(String city){
+    ArrayList<Car> car2 = new ArrayList<Car>();
+    for (Car c: cars){
+        if (c.getCity()== city){
+        car2.add(c);
+        }
+    }
+    return car2;
+    
+}
+    
+    
+    //Get All Cars with given number of seats
+    
+    public ArrayList<Car> getCarsBySeatsNumber(int minSeat , int maxSeat){
+    ArrayList<Car> car2 = new ArrayList<Car>();
+    for (Car c: cars){
+        if (c.getSeatsNumber() >= minSeat && c.getSeatsNumber() < maxSeat){
+        car2.add(c);
+        }
+        return car2;
+    }
+    return null;
+    
+}
+    
+    //List All Cars by Expired Maintainance Certificate
+  /*  
+    public ArrayList<Car> getCarsByManufacturedYear(){
+    ArrayList<Car> car2 = new ArrayList<Car>();
+    
+    Date date = new Date();
+    
+    for (Car c: cars){
+        if (c.getMaintainanceCertificateDate().compareTo(date) < 0){
+        car2.add(c);
+        }
+        return car2;
+    }
+    return null;
+    
+}
+    */
+    
+  //Last Fleet update time
+    
+/*   public Date getLastUpdateTime(){
+        
+    
+    
+    Date date = new Date(0, 0, 0 , 0, 0 ,0);
+    
+    for (Car c: cars){
+    
+        if (c.getMaintainanceCertificateDate().compareTo(date)> 0){
+        date = c.getMaintainanceCertificateDate();
+        }
+        return date;
+    }
+    return null;
+    
+} */
+    // List All Car Manufacturers
+
+    /**
+     *
+     * @return
+     */
+    
+    public HashSet<Integer> getModels(){
+    
+        
+        HashSet<Integer> hs = new HashSet<>();
+
+    for (Car c: cars){
+        {
+        hs.add(c.getModelNumber());
+        }
+        return hs;
+    }
+    return null;
+    
+}
+    
+}
